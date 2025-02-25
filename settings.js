2085f9775860131225af1bb150959628962e9911
Gabrieleoq <givanovcostin@gmail.com>
2025-02-25 21:00:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=pDFzhaDZ#0ixcvQ5_EPuRKHyrZRF8sG5dj73_dcKlsuGzgn4mB_4' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=BG0k3DIa#tPZhmqfqAhR16G3GBuQtaEBIBPIS5OGZvFaWRdEKRB0' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
